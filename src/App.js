468318f96d64d254a8e52dfdeaadb4f602276c91
Andres Järviste <andres.jarviste@wbscodingschool.com>
2022-08-01 20:49:27 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import Button from '@mui/material/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button className="myColors" variant="contained">By me a coffee!</Button>
+      <Button variant="text">By me a second coffee!</Button>
+      <Button color="error" variant="outlined" size="large">By me a third coffee!</Button>
+      
     </div>
   );
 }
